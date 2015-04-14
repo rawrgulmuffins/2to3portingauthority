71da72bbff761edd1ae6a486f2759b677e7c7f13
Alex Lord <lostdogs101@gmail.com>
2015-04-14 17:14:11 -0400

    Added logging messages and unit tests.

--- python_porting_authority_cli/python_porting_authority.py
+++ python_porting_authority_cli/python_porting_authority.py
@@ -12,22 +12,31 @@ except ImportError:
 from random import randint
 import logging
 
+SERVER_PROXY_ERROR_MSG = "Wasn't able to connect to '{}'. Giving up."
+EMPTY_PYTHON3_RETURN = ("Retrieved no package names from pypi which are"
+    "python3 ready.")
+EMPTY_2_AND_3_RETURN = ("Recieved no python packages or python3 ready packages. "
+    "Most likely passed an empty pypi server.")
+WAS_3_NOTHING_IN_COMPLETE_LIST = ("Recieved no python packages after getting"
+    "python3 ready packages. Most likely race condition error. See if error "
+    "happens again.")
+
 def column(matrix, i):
     """
     Found at http://stackoverflow.com/questions/903853/how-to-extract-column-from-a-multi-dimentional-array
     """
     return [row[i] for row in matrix]
 
-def get_xmlrpc_client(logger, pypi_url="https://pypi.python.org/pypi"):
+def get_xmlrpc_client(pypi_url="https://pypi.python.org/pypi"):
     """
     Retrieve a pypi xmlrpc client connection by which you can query package
     information from pypi.
     """
+    logger = logging.getLogger("python_pa")
     try:
         return xmlrpclib.ServerProxy(pypi_url)
-    except xmlrpc.client.ProtocolError:
-        logger.critical("Wasn't able to connect to '{}'. Giving up.".format(
-            pypi_url))
+    except (xmlrpclib.ProtocolError, OSError):
+        logger.critical(SERVER_PROXY_ERROR_MSG.format(pypi_url))
 
 def get_python2_package_names(xmlrpc_client):
     """
@@ -35,15 +44,25 @@ def get_python2_package_names(xmlrpc_client):
     all the package names. Return a list of all package names - python3 ready
     packages.
     """
+    logger = logging.getLogger("python_pa")
     classifiers=["Programming Language :: Python :: 3"]
     python3_package_names_versions = xmlrpc_client.browse(classifiers)
     python3_package_names = set(column(python3_package_names_versions, 0))
+    if python3_package_names == set([]):
+        logger.warning(EMPTY_PYTHON3_RETURN)
     all_package_names = set(xmlrpc_client.list_packages())
+    if all_package_names == set([]):
+        if python3_package_names == set([]):
+            # Probably a broken pypi server.
+            logger.warning(EMPTY_2_AND_3_RETURN)
+        else:
+            # Something died in the matrix.
+            logger.critical(WAS_3_NOTHING_IN_COMPLETE_LIST)
     return list(all_package_names.difference(python3_package_names))
 
 def main():
     logger = logging.getLogger("python_pa")
-    xmlrpc_client = get_xmlrpc_client(logger)
+    xmlrpc_client = get_xmlrpc_client()
     python2_package_names = get_python2_package_names(xmlrpc_client)
     package_index = randint(0, len(python2_package_names))
     selected_package_name = python2_package_names[package_index]
--- python_porting_authority_cli/test.py
+++ python_porting_authority_cli/test.py
@@ -1,42 +1,109 @@
 import unittest
+from unittest.mock import MagicMock
+import logging
+try:
+    import xmlrpclib
+except ImportError:
+    import xmlrpc.client as xmlrpclib
 
-from testfixtures import log_capture
+from testfixtures import LogCapture
 
 import python_porting_authority as python_pa
 
 class TestPortingAuthorityMiscFunctions(unittest.TestCase):
 
-    @log_capture
     def test_xmlrpc_client_bad_url(self):
-        python_pa.get_xml_rpc(pypi_url="i'm a bad url")
-        l.check("2to3_pa", "CRITICAL", "Wasn't able to connect to '{}'. Giving up.")
+        with LogCapture() as l:
+            bad_pypi_url = "I'm a very bad url"
+            python_pa.get_xmlrpc_client(pypi_url=bad_pypi_url)
+            expected_logs = (
+                (
+                    "python_pa",
+                    "CRITICAL",
+                    "Wasn't able to connect to '{}'. Giving up.".format(
+                        bad_pypi_url)))
+            l.check(expected_logs)
 
-    def test_xmlrpc_client_bad_url(self):
+    def test_xmlrpc_client_default_url(self):
         """
         This test requires the interwebs.  Could fail for many valid reasons.
         """
-        #2to3pa.get_xml_rpc(pypi_url="i'm a bad url")
-        pass
+        xmlrpc_client = python_pa.get_xmlrpc_client()
+        self.assertEqual(type(xmlrpc_client), xmlrpclib.ServerProxy)
+
+    def test_xmlrpc_client_good_passed_url(self):
+        """
+        This test requires the interwebs.  Could fail for many valid reasons.
+        """
+        xmlrpc_client = python_pa.get_xmlrpc_client(
+            pypi_url="https://pypi.python.org/pypi")
+        self.assertEqual(type(xmlrpc_client), xmlrpclib.ServerProxy)
 
     def test_column_not_matrix(self):
-        pass
+        test_list = []
+        actual_list = python_pa.column(test_list, 0)
+        expected_list = []
+        self.assertEqual(expected_list, actual_list)
 
     def test_column_index_out_of_range(self):
-        pass
+        test_list = [[1, 2], [3, 4]]
+        with self.assertRaises(IndexError):
+            python_pa.column(test_list, 2)
 
 class TestGetPython2PackageNames(unittest.TestCase):
-    
+
     def test_browse_returns_nothing(self):
         """
-        XMLRPC's can have server errors that lead to no data. Handle that case
+        XMLRPC's can have server errors that lead to no data. If we get no
+        python packages that are three ready log a warning.
         """
-        pass
+        with LogCapture() as l:
+            xmlrpc_client = xmlrpclib.ServerProxy
+            xmlrpc_client.browse = MagicMock(return_value=[])
+            xmlrpc_client.list_packages = MagicMock(return_value=["a_package"])
+            python_pa.get_python2_package_names(xmlrpc_client)
+            expected_logs = (
+                (
+                    "python_pa",
+                    "WARNING",
+                    python_pa.EMPTY_PYTHON3_RETURN))
+            l.check(expected_logs)
 
-    def test_list_packages_returns_nothing(self):
+    def test_list_packages_returns_nothing_after_getting_3_packages(self):
         """
-        XMLRPC's can have server errors that lead to no data. Handle that case
+        XMLRPC's can have server errors that lead to no data. If we got python3
+        packages but nothing during the complete list request, log a critical
+        warning and exit.
         """
-        pass
+        with LogCapture() as l:
+            xmlrpc_client = xmlrpclib.ServerProxy
+            xmlrpc_client.browse = MagicMock(return_value=["python3_ready"])
+            xmlrpc_client.list_packages = MagicMock(return_value=[])
+            python_pa.get_python2_package_names(xmlrpc_client)
+            expected_logs = (
+                (
+                    "python_pa",
+                    "CRITICAL",
+                    python_pa.WAS_3_NOTHING_IN_COMPLETE_LIST))
+            l.check(expected_logs)
+
+    def test_list_packages_returns_nothing_after_getting_no_3_packages(self):
+        """
+        XMLRPC's can have server errors that lead to no data. If we recieved
+        no python3 then log a warning about getting no python packages at
+        all.
+        """
+        with LogCapture() as l:
+            xmlrpc_client = xmlrpclib.ServerProxy
+            xmlrpc_client.browse = MagicMock(return_value=[])
+            xmlrpc_client.list_packages = MagicMock(return_value=[])
+            python_pa.get_python2_package_names(xmlrpc_client)
+            expected_logs = [
+                ("python_pa", "WARNING", python_pa.EMPTY_PYTHON3_RETURN,),
+                ("python_pa", "WARNING", python_pa.EMPTY_2_AND_3_RETURN,)]
+            print(l)
+            print(expected_logs)
+            l.check(*expected_logs)
 
     def test_pypi_api_changes(self):
         """
