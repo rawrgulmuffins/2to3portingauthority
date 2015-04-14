2372c802b58811e1a779dfacd81245864ac3ff58
Alex Lord <lostdogs101@gmail.com>
2015-04-14 14:03:29 -0400

    Have a minimal viable product. Returns a random package which is only 2 only.

--- porting_authority_cli.py
+++ porting_authority_cli.py
@@ -6,7 +6,7 @@ try:
     import xmlrpclib
 except ImportError:
     import xmlrpc.client as xmlrpclib
-import pprint
+from random import randint
 
 def column(matrix, i):
     return [row[i] for row in matrix]
@@ -15,8 +15,13 @@ def main():
     client = xmlrpclib.ServerProxy("https://pypi.python.org/pypi")
     classifiers=["Programming Language :: Python :: 3"]
     python3_package_names_versions = client.browse(classifiers)
-    python3_package_names = column(python3_package_names_versions, 0)
-    pprint.pprint(python3_package_names)
+    python3_package_names = set(column(python3_package_names_versions, 0))
+    all_package_names = set(client.list_packages())
+    python2_only_package_name = all_package_names.difference(
+        python3_package_names)
+    package_index = randint(0, len(python2_only_package_name))
+    selected_package_name = list(python2_only_package_name)[package_index]
+    print(selected_package_name)
 
 if __name__ == "__main__":
     main()
